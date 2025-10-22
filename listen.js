b43e51bd1b2ffe10e375a74ff0e29b8b736e586d
APigott1 <pigster99@gmail.com>
2025-10-22 10:59:50 +0100

    removed reference to undefined port variable

--- listen.js
+++ listen.js
@@ -4,6 +4,6 @@ app.listen(9090, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port 9090`);
   }
 });
