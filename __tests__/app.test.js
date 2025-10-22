06d901a94540b8abb69b953283804979591430d0
APigott1 <pigster99@gmail.com>
2025-10-22 11:02:23 +0100

    added test for GET endpoint at /api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,25 @@
-describe("placeholder", () => {
-  test("placeholder", () => {});
+const request = require("supertest");
+const app = require("../app.js");
+const seed = require("../db/seeds/seed.js");
+const data = require("../db/data/test-data/index.js");
+const db = require("../db/connection.js");
+
+beforeEach(async () => {
+  await seed(data);
+});
+
+afterAll(async () => {
+  await db.end();
+});
+
+describe("GET /api/topics", () => {
+  test("200: returns an object with an array of topic assigned to a key topics", async () => {
+    const { body } = await request(app).get("/api/topics").expect(200);
+
+    body.topics.forEach((topic) => {
+      expect(typeof topic.slug).toBe("string");
+      expect(typeof topic.description).toBe("string");
+      expect(typeof topic.img_url).toBe("string");
+    });
+  });
 });
